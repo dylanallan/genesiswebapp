4c557ed62b84d4bf3f357eca764b64dd392f5ee7
dylanallan <dylltoamill@gmail.com>
2025-05-30 11:04:39 -0400

    Fix Google Sign In Implementation

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -33,12 +33,11 @@ export const Auth: React.FC = () => {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
+          redirectTo: `${window.location.origin}`,
           queryParams: {
             access_type: 'offline',
             prompt: 'consent',
-            client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID
           },
-          redirectTo: `${window.location.origin}/auth/callback`
         }
       });
 
@@ -67,7 +66,7 @@ export const Auth: React.FC = () => {
             email: data.email,
             password: data.password,
             options: {
-              emailRedirectTo: `${window.location.origin}/auth/callback`
+              emailRedirectTo: `${window.location.origin}`
             }
           });
 
