3d0887ac7339e69faf5822e31c11133bbe042cd3
dylanallan <dylltoamill@gmail.com>
2025-05-31 08:54:57 -0400

    Fix Google Authentication

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -33,11 +33,7 @@ export const Auth: React.FC = () => {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.origin,
-          queryParams: {
-            access_type: 'offline',
-            prompt: 'consent',
-          }
+          redirectTo: window.location.origin
         }
       });
 
