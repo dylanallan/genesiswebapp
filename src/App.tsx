b77ad4b3ef0a4b7b0c0a6b0ef5a027c0b8839474
dylanallan <dylltoamill@gmail.com>
2025-06-03 23:11:59 -0400

    Fix white screen issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useUser } from '@supabase/auth-helpers-react';
+import { Auth } from './components/Auth';
 import { Dashboard } from './components/Dashboard';
 
 function App() {
+  const user = useUser();
+
+  if (!user) {
+    return <Auth />;
+  }
+
   return <Dashboard />;
 }
 
