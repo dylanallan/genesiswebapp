bac9ee3a402262991ee402c538c0b05f130e2015
dylanallan <dylltoamill@gmail.com>
2025-06-24 22:40:35 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
-  at new Promise (https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3.w-credentialless-staticblitz.com/blitz.b980e309.js:31:27909)
-    at requestCallbacks.on-end (/home/project/node_modules/vite/node_modules/esbuild/lib/main.js:944:54)
-    at handleRequest (/home/project/node_modules/vite/node_modules/esbuild/lib/main.js:637:17)
+     import React from "react";
+   import { MainApp } from "./components/MainApp";
+
+   export default function App() {
+     return <MainApp />;
+   }
